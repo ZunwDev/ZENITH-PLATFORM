571c2354e733d6e2ddc172a451c38f76b7e6bbe2
Zunw <43964753+ZunwDev@users.noreply.github.com>
2024-02-22 03:00:09 +0100

    feat: Added image manager into product adding form

---
+++ frontend/src/components/dashboard/products/components/ProductImageManager.tsx
@@ -0,0 +1,145 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import ImageUploader from "@/components/util/ImageUploader";
+import { cn } from "@/lib/utils";
+import { BoxSelect, Trash, InfoIcon } from "lucide-react";
+import { useCallback } from "react";
+
+interface ImageManagerProps {
+  images: string[];
+  imageThumbnail: string;
+  selectedImages: string[];
+  setImages: React.Dispatch<React.SetStateAction<string[]>>;
+  setImageThumbnail: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedImages: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+export default function ProductImageManager({
+  images,
+  imageThumbnail,
+  selectedImages,
+  setImages,
+  setImageThumbnail,
+  setSelectedImages,
+}: ImageManagerProps) {
+  const deleteSelectedImages = useCallback(() => {
+    setImages((prev) => prev.filter((item) => !selectedImages.includes(item)));
+    setSelectedImages([]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedImages]);
+
+  const selectAllImages = useCallback(() => {
+    setSelectedImages((prev) => (prev.length === images.length ? [] : images));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [images]);
+
+  const setImageAsThumbnail = useCallback((image: string) => {
+    setImageThumbnail(image);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleImageSelect = useCallback((image: string) => {
+    setSelectedImages((prev) => (prev.includes(image) ? prev.filter((item) => item !== image) : [...prev, image]));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Card className="md:w-80 w-full h-fit border rounded-md flex flex-col flex-shrink-0">
+      <CardHeader>
+        <CardTitle>Product Images</CardTitle>
+        <CardDescription>Upload images that will represent the product.</CardDescription>
+      </CardHeader>
+      <CardContent className="h-fit">
+        <>
+          {images.length > 0 ? (
+            <ScrollArea className="h-96">
+              <div className="grid md:grid-cols-3 sm:grid-cols-5 grid-cols-4 gap-2 p-2 overflow-y-auto h-full">
+                {images.map((item, index) => (
+                  <div className="relative group" key={index}>
+                    <img
+                      title="Click to set as thumbnail"
+                      src={item}
+                      loading="lazy"
+                      width={96}
+                      height={96}
+                      className={cn("rounded-md object-cover cursor-pointer ring-2 ring-accent", {
+                        "ring-2 ring-primary": item === imageThumbnail,
+                        "group-hover:ring-primary/30": item !== imageThumbnail,
+                      })}
+                      onClick={() => setImageAsThumbnail(item)}
+                    />
+                    <Checkbox
+                      checked={selectedImages.includes(item)}
+                      className={cn(
+                        "absolute top-1 right-1 bg-background",
+                        { "group-hover:flex hidden": selectedImages.length == 0 },
+                        { flex: selectedImages.length > 0 }
+                      )}
+                      onClick={() => handleImageSelect(item)}></Checkbox>
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          ) : (
+            <div className="flex justify-center w-full items-center h-96">No images yet...</div>
+          )}
+        </>
+      </CardContent>
+      <Separator />
+      <CardFooter className="text-sm mx-2 gap-2 p-2 flex flex-col items-start">
+        <div className="flex flex-row gap-1.5 items-center w-full justify-between">
+          <div className="space-x-1.5">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    className="p-1 size-8"
+                    name="selectAll"
+                    disabled={images.length == 0}
+                    variant={
+                      images.length && selectedImages.length > 0 && images.length == selectedImages.length
+                        ? "destructive"
+                        : undefined
+                    }
+                    onClick={selectAllImages}>
+                    <BoxSelect className="size-5" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  {images.length == selectedImages.length ? <p>Unselect all images</p> : <p>Select all images</p>}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    className="p-1 size-8 group"
+                    name="deleteSelected"
+                    variant="outline"
+                    disabled={selectedImages.length == 0}
+                    onClick={deleteSelectedImages}>
+                    <Trash className="size-5 group-hover:text-red-500" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Delete all selected</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+          <span className="text-text">{`Selected ${selectedImages.length} of ${images.length}`}</span>
+        </div>
+        <ImageUploader setImages={setImages} />
+        <div className="flex flex-row items-center pb-1">
+          <InfoIcon className="inline-block text-primary h-4 w-4" />
+          <span className="ml-1 text-primary">Blue border = thumbnail</span>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ frontend/src/components/util/ImageUploader.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Label } from "../ui/label";
+
+interface ImageUploaderProps {
+  setImages: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const ImageUploader = ({ setImages }: ImageUploaderProps) => {
+  const [dragging, setDragging] = useState(false);
+  const [invalidFileType, setInvalidFileType] = useState(false);
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(false);
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(false);
+    const files = Array.from(e.dataTransfer.files);
+    handleFiles(files);
+  };
+
+  const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files);
+    handleFiles(files);
+  };
+
+  const handleFiles = (files: File[]) => {
+    const imageTypes = ["image/png", "image/jpeg", "image/jpg"];
+    const validFiles = files.filter((file) => imageTypes.includes(file.type));
+
+    if (validFiles.length !== files.length) {
+      setInvalidFileType(true);
+    } else {
+      setInvalidFileType(false);
+    }
+
+    const newImageURLs = validFiles.map((file) => URL.createObjectURL(file));
+
+    setImages((prevImages) => [...prevImages, ...newImageURLs]);
+  };
+
+  return (
+    <div
+      className={`border border-dashed border-gray-500 py-4 px-2 mt-auto rounded-lg overflow-hidden flex flex-col justify-center items-center space-y-2 h-64 w-full ${
+        dragging ? "bg-gray-200" : ""
+      }`}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}>
+      <div className="text-center space-y-2">
+        <p className="text-sm text-gray-500 flex flex-col">
+          <span>Drag & drop your images here or</span>
+          <Label className="underline cursor-pointer" htmlFor="file">
+            browse your device
+          </Label>
+          {invalidFileType && <span className="text-red-500">We only support PNG, JPG and JPEG images.</span>}
+        </p>
+      </div>
+      <input className="sr-only" id="file" multiple type="file" accept=".png, .jpg, .jpeg" onChange={handleFileInputChange} />
+    </div>
+  );
+};
+
+export default ImageUploader;
