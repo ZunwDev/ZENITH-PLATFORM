e9bf02501dce437ef0babdec877f0f3ac0937e2e
Zunw <43964753+ZunwDev@users.noreply.github.com>
2024-02-24 14:17:36 +0100

    feat: Added code editor for product specs in JSON inside the form, with error checks and templates based on selected category

--- frontend/src/components/dashboard/forms/NewProductForm.tsx
+++ frontend/src/components/dashboard/forms/NewProductForm.tsx
@@ -1,6 +1,6 @@
 import { Form } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Brand, Category } from "../products/interfaces";
@@ -9,6 +9,13 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import ProductImageManager from "../products/components/ProductImageManager";
 import { InputFormItem, SelectFormItem, TextareaFormItem } from "@/components/util/FormItems";
 import PageHeader from "@/components/global/PageHeader";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
+import { json } from "@codemirror/lang-json";
+import CodeMirror from "@uiw/react-codemirror";
+import { githubLight } from "@uiw/codemirror-theme-github";
+import { templates } from "@/lib/enum/specsTemplates";
+import { Button } from "@/components/ui/button";
 
 const FormSchema = z.object({
   name: z.string().min(1, {
@@ -38,6 +45,18 @@ const FormSchema = z.object({
   }),
 });
 
+const categoryTemplateMapping = {
+  "computers & laptops": templates.ComputersAndLaptops,
+  "audio & headphones": templates.AudioAndHeadphones,
+  "cameras & photography": templates.CamerasAndPhotography,
+  "wearable technology": templates.WearableTechnology,
+  "home electronics": templates.HomeElectronics,
+  "gaming & consoles": templates.GamingAndConsoles,
+  "cables & adapters": templates.CablesAndAdapters,
+  "power banks & chargers": templates.PowerBanksAndChargers,
+  "smartphones & accessories": templates.SmartphonesAndAccessories,
+};
+
 export default function NewProductForm() {
   //Data
   const [categories, setCategories] = useState<Category[]>([]);
@@ -53,6 +72,11 @@ export default function NewProductForm() {
   const [images, setImages] = useState([]);
   const [selectedImages, setSelectedImages] = useState([]);
   const [imageThumbnail, setImageThumbnail] = useState("");
+
+  //Specs
+  const [jsonData, setJsonData] = useState("");
+  const [formattedJSON, setFormattedJSON] = useState("");
+  const [parseError, setParseError] = useState<string | null>(null);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -73,18 +97,48 @@ export default function NewProductForm() {
     setBrands(brandData);
   };
 
+  const onChange = useCallback((val) => {
+    setJsonData(val);
+  }, []);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      try {
+        JSON.parse(jsonData);
+        setFormattedJSON(JSON.stringify(JSON.parse(jsonData), null, 2));
+        setParseError(null);
+      } catch (error) {
+        setParseError("Invalid JSON format: " + error.message);
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [jsonData]);
+
   useEffect(() => {
     fetchData();
   }, []);
 
   useEffect(() => {
     setImageThumbnail(images[0]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [images]);
 
-  //const handleNewProjectClick = async (values: z.infer<typeof FormSchema>) => {};
+  //if category changed, set corresponding jsonData template
+  useEffect(() => {
+    if (categoriesSelectedValue && categoriesSelectedValue in categoryTemplateMapping) {
+      setJsonData(categoryTemplateMapping[categoriesSelectedValue]);
+    } else {
+      setJsonData("");
+    }
+  }, [categoriesSelectedValue]);
+
+  const handleNewProjectClick = async (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
+  };
 
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-16 pb-32">
       <div className="md:px-0 px-4 flex justify-between flex-row border-b pb-4">
         <PageHeader title="New Product" description="Add new product to the store" />
       </div>
@@ -100,90 +154,117 @@ export default function NewProductForm() {
           />
         </div>
 
-        <Card className="w-full border h-fit rounded-md">
-          <CardHeader>
-            <CardTitle>Product Information</CardTitle>
-            <CardDescription>Enter key product details for comprehensive information.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form className="flex md:flex-row md:gap-20 gap-4 flex-col">
-                <div className="flex flex-col md:w-1/3 w-full gap-4">
-                  <InputFormItem
-                    label="Product Name"
-                    id="name"
-                    placeholder="HP EliteBook 650 G10"
-                    form={form}
-                    required
-                    description="Enter the name of the product."></InputFormItem>
-                  <TextareaFormItem
-                    label="Product Description"
-                    id="description"
-                    form={form}
-                    required
-                    description="Briefly describe the product and its main features."
-                    placeholder='Notebook - Intel Core i5 1345U Raptor Lake, 15.6" IPS anti-glare 1920×1080, RAM 16GB DDR4, Intel Iris Xe Graphics, SSD 512GB, numeric keypad, backlit keypad, webcam, USB 3.2 Gen 1, USB-C, fingerprint reader, WiFi 6E, WiFi, Bluetooth, Weight 1.78 kg, Windows 11 Pro'></TextareaFormItem>
-                </div>
-                <div className="flex flex-col md:w-1/4 w-full gap-4">
-                  <InputFormItem
-                    label="Price"
-                    id="price"
-                    type="number"
-                    placeholder="49.99"
-                    required
-                    description="Specify the price of the product. Always end the price with 9 (e.g., 49)."
-                    form={form}
-                    prefix="$"></InputFormItem>
-                  <InputFormItem
-                    label="Discount"
-                    id="discount"
-                    type="number"
-                    placeholder="20"
-                    description="Set the discount percentage for the product (e.g., 20 for 20% off)."
-                    form={form}
-                    suffix="%"></InputFormItem>
-                  <InputFormItem
-                    label="Quantity"
-                    id="quantity"
-                    type="number"
-                    placeholder="100"
-                    required
-                    description="Enter the quantity of the product in stock."
-                    form={form}></InputFormItem>
-                </div>
-                <div className="flex flex-col md:w-1/4 w-full gap-4">
-                  <SelectFormItem
-                    label="Category"
-                    id="category"
-                    placeholder="Search categories..."
-                    description="Select corresponding category to the product."
-                    required
-                    form={form}
-                    data={categories}
-                    open={openCategory}
-                    setOpen={setOpenCategory}
-                    selectedValue={categoriesSelectedValue}
-                    setSelectedValue={setCategoriesSelectedValue}></SelectFormItem>
-                  <SelectFormItem
-                    label="Brand"
-                    id="brand"
-                    placeholder="Search brands..."
-                    description="Select corresponding brand to the product."
-                    required
-                    form={form}
-                    data={brands}
-                    open={openBrands}
-                    setOpen={setOpenBrands}
-                    selectedValue={brandsSelectedValue}
-                    setSelectedValue={setBrandsSelectedValue}></SelectFormItem>
-                </div>
-                {/*               <Button type="submit" onClick={form.handleSubmit(handleNewProjectClick)} className="mt-4">
-                Create
-              </Button> */}
-              </form>
-            </Form>
-          </CardContent>
-        </Card>
+        <div className="flex flex-col gap-8">
+          <Card className="w-full border h-fit rounded-md">
+            <CardHeader>
+              <CardTitle>Product Information</CardTitle>
+              <CardDescription>Enter key product details for comprehensive information.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Form {...form}>
+                <form className="flex md:flex-row md:gap-20 gap-4 flex-col">
+                  <div className="flex flex-col md:w-1/3 w-full gap-4">
+                    <InputFormItem
+                      label="Product Name"
+                      id="name"
+                      placeholder="HP EliteBook 650 G10"
+                      form={form}
+                      required
+                      description="Enter the name of the product."></InputFormItem>
+                    <TextareaFormItem
+                      label="Product Description"
+                      id="description"
+                      form={form}
+                      required
+                      description="Briefly describe the product and its main features."
+                      placeholder='Notebook - Intel Core i5 1345U Raptor Lake, 15.6" IPS anti-glare 1920×1080, RAM 16GB DDR4, Intel Iris Xe Graphics, SSD 512GB, numeric keypad, backlit keypad, webcam, USB 3.2 Gen 1, USB-C, fingerprint reader, WiFi 6E, WiFi, Bluetooth, Weight 1.78 kg, Windows 11 Pro'></TextareaFormItem>
+                  </div>
+                  <div className="flex flex-col md:w-1/4 w-full gap-4">
+                    <InputFormItem
+                      label="Price"
+                      id="price"
+                      type="number"
+                      placeholder="49.99"
+                      required
+                      description="Specify the price of the product. Always end the price with 9 (e.g., 49)."
+                      form={form}
+                      prefix="$"></InputFormItem>
+                    <InputFormItem
+                      label="Discount"
+                      id="discount"
+                      type="number"
+                      placeholder="20"
+                      description="Set the discount percentage for the product (e.g., 20 for 20% off)."
+                      form={form}
+                      suffix="%"></InputFormItem>
+                    <InputFormItem
+                      label="Quantity"
+                      id="quantity"
+                      type="number"
+                      placeholder="100"
+                      required
+                      description="Enter the quantity of the product in stock."
+                      form={form}></InputFormItem>
+                  </div>
+                  <div className="flex flex-col md:w-1/4 w-full gap-4">
+                    <SelectFormItem
+                      label="Category"
+                      id="category"
+                      placeholder="Search categories..."
+                      description="Select corresponding category to the product."
+                      required
+                      form={form}
+                      data={categories}
+                      open={openCategory}
+                      setOpen={setOpenCategory}
+                      selectedValue={categoriesSelectedValue}
+                      setSelectedValue={setCategoriesSelectedValue}></SelectFormItem>
+                    <SelectFormItem
+                      label="Brand"
+                      id="brand"
+                      placeholder="Search brands..."
+                      description="Select corresponding brand to the product."
+                      required
+                      form={form}
+                      data={brands}
+                      open={openBrands}
+                      setOpen={setOpenBrands}
+                      selectedValue={brandsSelectedValue}
+                      setSelectedValue={setBrandsSelectedValue}></SelectFormItem>
+                  </div>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Product Specs</CardTitle>
+              <CardDescription>
+                Specify product specifications here. Ensure consistency across all products. These templates demonstrate the
+                content each category can include.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              {jsonData !== "" && parseError && (
+                <Alert variant="destructive">
+                  <AlertCircle className="size-5" />
+                  <AlertDescription>{parseError}</AlertDescription>
+                </Alert>
+              )}
+              <CodeMirror
+                value={formattedJSON}
+                theme={githubLight}
+                placeholder="Select any category, to get an editable template."
+                height="252px"
+                extensions={[json()]}
+                onChange={onChange}
+              />
+            </CardContent>
+          </Card>
+          <Button type="submit" onClick={form.handleSubmit(handleNewProjectClick)} className="w-24 ml-auto">
+            Create
+          </Button>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/lib/enum/specsTemplates.ts
@@ -0,0 +1,326 @@
+export const templates = {
+  ComputersAndLaptops: `{
+        "KEY SPECS": {
+          "Battery Life": "8.5 hours +",
+          "Computer Operating System": "Google Chrome",
+          "Hard Drive Type": "eMMC",
+          "Installed RAM": "4GB",
+          "Processor Type": "Intel Celeron",
+          "SSD Capacity": "64GB",
+          "Screen Resolution": "1366 x 768",
+          "Screen Size (inches)": 14,
+          "Weight (lbs.)": 3.24
+        },
+        "CONNECTIVITY": {
+          "Bluetooth Compatible": "Yes",
+          "Media Card Reader": "microSD",
+          "Network Card": "Dual-band Wi-Fi 6 (802.11ax, 2 x 2)/Bluetooth 5.0",
+          "Number of DVI Ports": 0,
+          "Number of Display Ports": 0,
+          "Number of HDMI Ports": 0,
+          "Number of USB Ports": 4,
+          "Number of Video Output Ports": 0,
+          "Wireless Connectivity": "802.11ax"
+        },
+        "DIMENSIONS": {
+          "Depth (Inches)": 9,
+          "Height (Inches)": 0.74,
+          "Laptop Weight": "3 to 3.9 lbs.",
+          "Width (Inches)": 12.85
+        },
+        "DISPLAY": {
+          "Computer Screen Display Type": "LED Backlit LCD"
+        },
+        "ENVIRONMENTAL": {
+          "ACMI AP nontoxic certified": "Yes",
+          "ENERGY STAR certified": "Yes",
+          "EPEAT verified": "EPEAT Silver verified",
+          "EcoID": "Yes, product meets Staples criteria for environmental features to earn the Eco-ID mark. Learn more at EcoIDProgram.com"
+        },
+        "FEATURES": {
+          "2-in-1": "Not 2-in-1",
+          "Backlit Keyboard": "Yes",
+          "Finger Print Reader": "No",
+          "Touch-Screen": "No Touch-Screen",
+          "Voice Assistant Built-In": "Google Assistant voice recognition support"
+        },
+        "GRAPHICS": {
+          "Computer Graphic Card Type": "Integrated",
+          "Graphics Processor": "Intel Celeron",
+          "Laptop Video Graphics": "Intel UHD Graphics"
+        },
+        "MEMORY": {
+          "CPU Cache Size": "4MB",
+          "Memory Type": "LPDDR4X SDRAM"
+        },
+        "POWER": {
+          "Laptop Battery Type": "3-cell, 50Wh lithium-ion"
+        },
+        "PROCESSOR": {
+          "Processor Core Generation": "Celeron N4500",
+          "Processor Speed (Ghz)": 1.1,
+          "Processor Speed (Up To Ghz)": "2 to 2.9 GHz"
+        },
+        "WARRANTY": {
+          "Warranty Information": "1 Year"
+        },
+        "ADDITIONAL DETAILS": {
+          "Color Family": "Gray/Silver",
+          "Computer Audio": "Built-in speaker/built-in microphone",
+          "Computer Use": "Business",
+          "Laptop Refurbished": "No",
+          "Laptop Style": "Chromebook"
+        },
+        "Series or Collection": "Chromebook CX1",
+        "True Color": "Transparent Silver"
+    }`,
+  AudioAndHeadphones: `{"KEY SPECS": {
+      "Bluetooth Compatible": "Yes",
+      "Built-In Microphone": "No",
+      "Color Family": "Black",
+      "Depth (Inches)": 3.2,
+      "Headphone Type": "Over-Ear",
+      "Height (Inches)": 7.1,
+      "Noise Canceling": "Noise Canceling",
+      "True Color": "Black",
+      "Warranty Information": "1 Year",
+      "Weight (lbs.)": 0.52,
+      "Width (Inches)": 6.7,
+      "Wireless or Wired": "Wireless"
+    }
+  }`,
+  CamerasAndPhotography: `{"KEY SPECS": {
+    "Camera Kit": "No",
+    "Camera or Camcorder Type": "Point & Shoot Camera",
+    "Color Family": "White",
+    "Digital": "Yes",
+    "LCD Size (Inches)": "3.0 or More",
+    "Megapixels": "20 Megapixels",
+    "Optical Zoom": "20x & Up",
+    "Rugged or Tough Camera": "Non Rugged",
+    "Series or Collection": "PIXPRO",
+    "True Color": "White",
+    "Waterproof": "Not Waterproof"
+    }
+  }`,
+  WearableTechnology: `{"KEY SPECS": {
+    "Bluetooth Compatible": "Yes",
+    "Depth (Inches)": 0.57,
+    "Diameter (Inches)": 0.57,
+    "Height (Inches)": 1.93,
+    "Material of Item": "Titanium Bonded",
+    "Screen Size in Millimeters": 49,
+    "Series or Collection": "Ultra 2 (GPS + Cellular)",
+    "Smart Watch & Accessory Type": "Smart Watch",
+    "Touch-Screen": "Touch-Screen",
+    "Tracker OS Compatibility": "iOS",
+    "True Color": "Blue/Black",
+    "Weight (Ounces)": 2.17,
+    "Width (Inches)": 1.73
+    }
+  }`,
+  HomeElectronics: `{"KEY SPECS": {
+    "Capacity (Cu. Ft.)": 4.7,
+    "Capacity in Cubic Feet or Number of Bottles": "4.1-5",
+    "Color Family": "Black",
+    "Depth (Inches)": 21.13,
+    "Energy Consumption per Year (kWh)": 331,
+    "Height (Inches)": 48.13,
+    "Number of Doors Drawers Taps": "2 Door",
+    "Power-Volts": 115,
+    "Refrigerator Door Compartments Qty": 3,
+    "Refrigerator Features": [
+    "Adjustable Temperature Control",
+    "Refrigerator Interior Lighting"
+    ],
+    "Refrigerator Shelves Qty": 3,
+    "Refrigerator Type": "Refrigerators w/Freezer",
+    "True Color": "Black/Stainless Steel",
+    "Warranty Information": "1 Year",
+    "Width (Inches)": 19
+    }
+  }`,
+  GamingAndConsoles: `{
+    "KEY SPECS": {
+    "Storage Size": "1,024 GB",
+    "Storage Type": "SSD",
+    "Console Type": "TV Console",
+    "4K Playback Support": "Native 4K",
+    "BLU-RAY Drive Type": "Blu-ray (4K)"},
+    "STORAGE": {
+      "Storage Size": "1,024 GB",
+      "Storage Type": "SSD"
+    },
+    "FEATURES": {
+    "Console Type": "TV Console",
+    "4K Playback Support": "Native 4K"
+    },
+    "VERSION": {"Version": "PS5"},
+    "PACKAGE CONTENTS": {"Package Contents": "Console, 1 Gaming Controller, HDMI Cable, USB Cable, Power Cable",
+    "Package Variant": "No Game Included"},
+    "DIMENSIONS AND WEIGHT": {
+    "Width": "9.6 cm",
+    "Height": "35.8 cm",
+    "Depth": "21.6 cm",
+    "Weight": "3.2 kg"
+    },
+    "PLATFORM": {"Platform": "PlayStation 5"},
+    "INPUTS AND OUTPUTS": {
+    "Connections": "3.5mm Jack (on controller), LAN (Ethernet), HDMI out, USB-A, USB-C, USB-C (on controller)"
+    },
+    "COLOR": {"Color": "White"},
+    "LOCALIZATION": {"Localized Menus": "Czech, Hungarian, English, German, French, Italian, Spanish"},
+    "SOCKET TYPE": {"Country": "Czech Republic, Slovakia, Hungary, Germany, Russia, Austria, Croatia, France, Poland, Romania, Slovenia"}
+    
+  }`,
+  CablesAndAdapters: `{"KEY SPECS": {
+    "Cable Length (Inches)": 59.06,
+    "Cable Length (ft)": 5,
+    "Cable Use": "Power",
+    "Color Family": "Black",
+    "Connector Gender": "Male to Male",
+    "End 1 Connector Type": "USB C",
+    "End 2 Connector Type": "USB C",
+    "Micro": "Yes",
+    "True Color": "Black",
+    "USB": "Yes"
+  }}`,
+  PowerBanksAndChargers: `{
+    "LENGTH & ENDS": {
+      "Cable Included": "Cable Included",
+      "Cable Length (Inches)": 6,
+      "Cable Length (ft)": 0.5
+    },
+    "TYPE & COMPATIBILITY": {
+      "# of Ports": 3,
+      "Brand Compatibility": "Multiple Brands",
+      "Charger or Charging Cable Type": "Power Bank",
+      "Device Port Type": "USB-C"
+    },
+    "WARRANTY": {
+      "Warranty Information": "5 Year"
+    },
+    "ADDITIONAL DETAILS": {
+      "Battery Capacity (mAh)": 10000,
+      "Features": ["LED indicator", "1-button control"],
+      "True Color": "Black",
+      "Watts": 18
+    }
+  }`,
+  SmartphonesAndAccessories: `{
+      "DISPLAY": {
+        "Screen Size (Inches)": "6.8",
+        "Display Resolution": "3088 × 1440",
+        "Operating System": "Android",
+        "Card Configuration": "Dual SIM + eSIM",
+        "Memory Card Slot": "No"
+      },
+      "BASIC PARAMETERS": {
+        "Screen Size (Inches)": "6.8",
+        "Display Resolution": "3088 × 1440",
+        "Operating System": "Android",
+        "Card Configuration": "Dual SIM + eSIM",
+        "Memory Card Slot": "No",
+        "Back Camera Resolution": "200 MP",
+        "Front Camera Resolution": "12 MP",
+        "Introduction Year": 2023
+      },
+      "FEATURES": {
+        "Fast Charging": true,
+        "Wireless Charging": true,
+        "Fast Wireless Charging": true,
+        "In-Display Fingerprint Reader": true,
+        "Reverse Wireless Charging": true,
+        "MMS Support": true,
+        "OTG Support": true,
+        "Desktop Mode (Samsung Dex)": true,
+        "Stylus Support": true
+      },
+      "SOCKET TYPE": {
+        "Country": ["Czech Republic", "Slovakia", "Hungary", "Germany", "Russia", "Austria", "Croatia", "France", "Poland", "Romania", "Slovenia"]
+      },
+      "CONSTRUCTION AND COLOR": {
+        "Type": "Touch Screen",
+        "Resistance": "Waterproof",
+        "IP Rating": "IP68",
+        "Gorilla Glass Version": "Gorilla Glass Victus 2",
+        "Height": "163.4 mm",
+        "Width": "78.1 mm",
+        "Depth": "8.9 mm",
+        "Weight": "233 g",
+        "Color": "Black"
+      },
+      "DISPLAY": {
+        "Screen Size (Inches)": "6.8 (17.27 cm)",
+        "Display Type": "AMOLED",
+        "Display Features": ["Gorilla Glass", "HDR 10+"],
+        "Display Refresh Rate": "120 Hz",
+        "Display Density": "500 PPI",
+        "Display Brightness": "1,750 Nits",
+        "Display Notch Shape": "Circular"
+      },
+      "CAMERA": {
+        "Camera Type": ["Wide Angle", "Telephoto"],
+        "Number of Rear Camera Lenses": 4,
+        "Number of Front Camera Lenses": 1,
+        "Main Rear Camera Resolution": "200 MP",
+        "Main Rear Camera Aperture": "f/1.7",
+        "Telephoto Camera Resolution": "10 MP",
+        "Telephoto Camera Aperture": "f/2.4",
+        "Second Telephoto Camera Resolution": "10 MP",
+        "Second Telephoto Camera Aperture": "f/4.9",
+        "Wide Angle Camera Resolution": "12 MP",
+        "Wide Angle Camera Aperture": "f/2.2",
+        "Front Camera Resolution": "12 MP",
+        "Front Camera Aperture": "f/2.2",
+        "Maximum Aperture": "f/1.7",
+        "Image Stabilization": "Optical",
+        "Optical Zoom": "10×",
+        "Maximum Video Resolution": "7680 × 4320 (8K Ultra HD)",
+        "Supported Resolutions and Frame Rates": {
+          "4320p (8K)": ["30fps", "24fps"],
+          "2160p (4K)": ["60fps", "30fps"],
+          "1080p (Full HD)": ["60fps", "30fps", "240fps", "960fps"],
+          "720p (HD)": ["30fps"]
+        },
+        "CAMERA FEATURES": ["Optical Zoom", "Flash LED"]
+      },
+      "CHARGING AND BATTERY LIFE": {
+        "Battery Capacity": "5,000 mAh",
+        "Max Wired Charging Power": "45 W",
+        "Max Wireless Charging Power": "15 W",
+        "Max Reverse Charging Power": "4.5 W",
+        "User Replaceable Battery": false
+      },
+      "MEMORY AND PERFORMANCE": {
+        "RAM": "12 GB",
+        "Internal Storage": "512 GB",
+        "Processor Brand": "Qualcomm",
+        "Processor Cores": 8,
+        "Processor Frequency": "3.36 GHz",
+        "Processor Model": "Qualcomm Snapdragon 8 Gen 2",
+        "Graphics Accelerator": "Adreno 740"
+      },
+      "COMMUNICATION AND NETWORK": {
+        "Wireless Technologies": ["NFC", "WiFi", "Bluetooth", "GPS", "UWB"],
+        "Data Services": ["5G", "LTE (4G)", "UMTS/CDMA (3G)"],
+        "Main SIM Card Type": "Nano SIM",
+        "Secondary SIM Card Type": "Nano SIM",
+        "Bluetooth Version": "5.3",
+        "Internet Calling": ["VoWiFi", "VoLTE"]
+      },
+      "CONNECTORS AND SENSORS": {
+        "Connectors": "USB-C",
+        "Sensors": ["Digital Compass", "Motion Sensor (Accelerometer, G-sensor)", "Gyroscope", "Light Sensor", "Proximity Sensor", "Barometer"]
+      },
+      "PACKAGE CONTENTS": {
+        "Included Accessories": "Excludes Charging Adapter (must be purchased separately)"
+      },
+      "SOFTWARE": {
+        "Operating System Version": "Android 13",
+        "User Interface": "Samsung One UI",
+        "Remote Management (MDM)": "Samsung Knox"
+      }
+    
+  }`,
+};
