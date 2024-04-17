8aa1c1c1eb3d61ac2f99fbe0c0f30dae4959e6cd
Zunw <43964753+ZunwDev@users.noreply.github.com>
2024-04-17 15:06:13 +0200

    style: Reformatted code in backend

--- src/main/java/dev/zunw/ecommerce/Brand/BrandRepository.java
+++ src/main/java/dev/zunw/ecommerce/Brand/BrandRepository.java
@@ -2,8 +2,6 @@ package dev.zunw.ecommerce.Brand;
 
 import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- src/main/java/dev/zunw/ecommerce/Status/StatusService.java
+++ src/main/java/dev/zunw/ecommerce/Status/StatusService.java
@@ -1,13 +1,9 @@
 package dev.zunw.ecommerce.Status;
 
-import dev.zunw.ecommerce.Product.Product;
-import dev.zunw.ecommerce.Product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static dev.zunw.ecommerce.ServiceUtils.getAllRows;
 
